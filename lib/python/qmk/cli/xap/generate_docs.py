daf8fd152ea5299dc2bcb47d4f967cff280f1e94
Nick Brassel <nick@tzarc.org>
2021-08-11 08:16:48 +1000

    Hidden if not developer mode.

--- lib/python/qmk/cli/xap/generate_docs.py
+++ lib/python/qmk/cli/xap/generate_docs.py
@@ -4,7 +4,7 @@ from milc import cli
 from qmk.xap.gen_docs.generator import generate_docs
 
 
-@cli.subcommand('Generates the XAP protocol documentation.')
+@cli.subcommand('Generates the XAP protocol documentation.', hidden=False if cli.config.user.developer else True)
 def xap_generate_docs(cli):
     """Generates the XAP protocol documentation by merging the definitions files, and producing the corresponding Markdown document under `/docs/`.
     """
--- lib/python/qmk/cli/xap/generate_qmk.py
+++ lib/python/qmk/cli/xap/generate_qmk.py
@@ -7,7 +7,7 @@ from qmk.xap.gen_firmware.generator import generate_inline, generate_header
 
 
 @cli.argument('-o', '--output', arg_only=True, type=normpath, help='File to write to')
-@cli.subcommand('Generates the XAP protocol include.')
+@cli.subcommand('Generates the XAP protocol include.', hidden=False if cli.config.user.developer else True)
 def xap_generate_qmk_inc(cli):
     """Generates the XAP protocol inline codegen file, generated during normal build.
     """
@@ -15,7 +15,7 @@ def xap_generate_qmk_inc(cli):
 
 
 @cli.argument('-o', '--output', arg_only=True, type=normpath, help='File to write to')
-@cli.subcommand('Generates the XAP protocol include.')
+@cli.subcommand('Generates the XAP protocol include.', hidden=False if cli.config.user.developer else True)
 def xap_generate_qmk_h(cli):
     """Generates the XAP protocol header file, generated during normal build.
     """
