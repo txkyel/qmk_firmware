4250197c260f646c371c35bf9414b104079edea5
Kyle Xiao <kylexiao20@gmail.com>
2021-08-11 13:52:59 -0400

    Add keymap for crkbd

---
+++ keyboards/crkbd/keymaps/txkyel/config.h
@@ -0,0 +1,86 @@
+/*
+This is the c configuration file for the keymap
+
+Copyright 2012 Jun Wako <wakojun@gmail.com>
+Copyright 2015 Jack Humbert
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+//#define USE_MATRIX_I2C
+
+/* Select hand configuration */
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+#define USE_SERIAL_PD2
+
+#define TAPPING_FORCE_HOLD
+
+#if defined RGBLIGHT_ENABLE
+#    undef RGBLED_NUM
+#    define RGBLIGHT_ANIMATIONS
+#    define RGBLED_NUM 27
+#    define RGBLIGHT_LIMIT_VAL 120
+#    define RGBLIGHT_HUE_STEP 10
+#    define RGBLIGHT_SAT_STEP 17
+#    define RGBLIGHT_VAL_STEP 17
+#endif
+
+// Rotate mouse
+#ifdef PS2_MOUSE_ENABLE
+#    define PS2_MOUSE_ROTATE 270
+#endif
+
+// Taken from https://github.com/joric/jorne/wiki/Trackpoint
+#ifdef PS2_USE_BUSYWAIT
+#    define PS2_CLOCK_PORT PORTD
+#    define PS2_CLOCK_PIN PIND
+#    define PS2_CLOCK_DDR DDRD
+#    define PS2_CLOCK_BIT 0
+#    define PS2_DATA_PORT PORTD
+#    define PS2_DATA_PIN PIND
+#    define PS2_DATA_DDR DDRD
+#    define PS2_DATA_BIT 1
+#endif
+
+// Taken from https://github.com/manna-harbour/qmk_firmware/blob/crkbd/keyboards/crkbd/keymaps/manna-harbour/readme.org
+#ifdef PS2_USE_INT
+#    define PS2_CLOCK_PORT PORTD
+#    define PS2_CLOCK_PIN PIND
+#    define PS2_CLOCK_DDR DDRD
+#    define PS2_CLOCK_BIT 2
+#    define PS2_DATA_PORT PORTD
+#    define PS2_DATA_PIN PIND
+#    define PS2_DATA_DDR DDRD
+#    define PS2_DATA_BIT 5
+
+#    define PS2_INT_INIT()                          \
+        do {                                        \
+            EICRA |= ((1 << ISC01) | (0 << ISC00)); \
+        } while (0)
+#    define PS2_INT_ON()          \
+        do {                      \
+            EIMSK |= (1 << INT0); \
+        } while (0)
+#    define PS2_INT_OFF()          \
+        do {                       \
+            EIMSK &= ~(1 << INT0); \
+        } while (0)
+#    define PS2_INT_VECT INT0_vect
+#endif
---
+++ keyboards/crkbd/keymaps/txkyel/keymap.c
@@ -0,0 +1,70 @@
+/*
+Copyright 2019 @foostan
+Copyright 2021 Kyle Xiao @txkyel
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include QMK_KEYBOARD_H
+#include "txkyel.h"
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  [0] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+TD(TH_ESC_TAB),TD(HC_Q),TD(HC_W),TD(HC_E),TD(HC_R),TD(HC_T),                    TD(HC_Y),TD(HC_U),TD(HC_I),TD(HC_O),TD(HC_P),TD(HC_BSPC),
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      KC_LCTL,TD(HC_A),TD(HC_S),TD(HC_D),TD(HC_F),TD(HC_G),                     TD(HC_H),TD(HC_J),TD(HC_K),TD(HC_L), KC_SCLN, KC_QUOT,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+       KC_ESC,TD(HC_Z),TD(HC_X),TD(HC_C),TD(HC_V),TD(HC_B),                     TD(HC_N),TD(HC_M), KC_COMM,  KC_DOT, KC_SLSH, KC_LGUI,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                          KC_LALT,  KC_SPC, KC_LGUI,    KC_LSFT,  OSL(1), KC_LALT
+                                      //`--------------------------'  `--------------------------'
+  ),
+  [1] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+      _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, XXXXXXX,   TO(3), _______,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      _______, KC_SLSH, KC_BSLS,  KC_GRV, KC_QUOT, KC_UNDS,                      KC_MINS, KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC,  KC_ENT,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      _______, XXXXXXX, KC_PIPE, KC_TILD,  KC_DQT, KC_PLUS,                       KC_EQL, KC_LCBR, KC_RCBR, _______, _______, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                            TO(0), _______, _______,    _______,   TO(2), _______
+                                      //`--------------------------'  `--------------------------'
+  ),
+
+  [2] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+   TD(HC_DEL),    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      _______, KC_SLSH, KC_BSLS,  KC_GRV, KC_QUOT, KC_UNDS,                      KC_MINS, KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC,  KC_ENT,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      _______, XXXXXXX, KC_PIPE, KC_TILD,  KC_DQT, KC_PLUS,                       KC_EQL, KC_LCBR, KC_RCBR, _______, _______, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                            TO(0), _______, _______,    _______, XXXXXXX, _______
+                                      //`--------------------------'  `--------------------------'
+  ),
+
+  [3] = LAYOUT_split_3x6_3(
+  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
+        RESET, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_HOME, KC_PGDN, KC_PGUP,  KC_END, _______,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                      XXXXXXX,TD(HC_LEFT),KC_DOWN,KC_UP,TD(HC_RGHT),KC_ENT,
+  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
+      RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
+                                            TO(0), _______, _______,    _______, XXXXXXX, _______
+                                      //`--------------------------'  `--------------------------'
+  )
+};
