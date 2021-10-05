aeef9778562bc97b54378dceb9dbc0911d3117ca
Kyle Xiao <kylexiao20@gmail.com>
2021-10-04 22:11:56 -0400

    Swap layer key positions

--- keyboards/ferris/keymaps/txkyel/keymap.c
+++ keyboards/ferris/keymaps/txkyel/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_BSPC,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLASH,
-                                   MO_NUM,  KC_SPC,      SFT_ENT, MO_NAV
+                                   MO_NAV,  KC_SPC,      SFT_ENT, MO_NUM
     ),
 
     [NAV] = LAYOUT(
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [NUM] = LAYOUT(
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
         KC_LCBR, KC_RCBR, KC_LPRN, KC_RPRN, KC_PLUS,     KC_MINS, OS_CTRL, OS_SHFT, OS_ALT,  OS_GUI,
-        KC_PIPE, KC_EQL,  KC_LBRC, KC_RBRC, KC_ASTR,     KC_SLSH, KC_QUOT, KC_COMM, KC_DOT,  KC_SCLN,
+        KC_BSLS, KC_EQL,  KC_LBRC, KC_RBRC, KC_ASTR,     KC_SLSH, KC_QUOT, KC_COMM, KC_DOT,  KC_SCLN,
                                    _______, _______,     _______, _______
     ),
 
