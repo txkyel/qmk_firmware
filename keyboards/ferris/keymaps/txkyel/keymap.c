220d4f40344a0cdb6533170f943ee850b08c8a13
Kyle Xiao <kylexiao20@gmail.com>
2021-10-04 21:18:45 -0400

    Swapping to QWERTY for adjustment to layout

--- keyboards/ferris/keymaps/txkyel/keymap.c
+++ keyboards/ferris/keymaps/txkyel/keymap.c
@@ -21,9 +21,9 @@ enum keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT(
-        KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,        KC_J,    KC_L,    KC_U,    KC_Y,    KC_BSPC,
-        KC_A,    KC_R,    KC_S,    KC_T,    KC_G,        KC_M,    KC_N,    KC_E,    KC_I,    KC_O,
-        KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,        KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH,
+        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,        KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,        KC_H,    KC_J,    KC_K,    KC_L,    KC_BSPC,
+        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,        KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLASH,
                                    MO_NUM,  KC_SPC,      KC_LSFT, MO_NAV
     ),
 
