2dcf046b198e2a3367f68b90bf09334f695c6de7
Kyle Xiao <kylexiao20@gmail.com>
2021-10-25 23:06:12 -0400

    Modify CRKBD keymap

--- keyboards/crkbd/keymaps/txkyel/config.h
+++ keyboards/crkbd/keymaps/txkyel/config.h
@@ -19,8 +19,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#define MASTER_LEFT
+// #define MASTER_LEFT
 // #define MASTER_RIGHT
+#define EE_HANDS
 
 #define TAPPING_FORCE_HOLD
 
--- keyboards/crkbd/keymaps/txkyel/keymap.c
+++ keyboards/crkbd/keymaps/txkyel/keymap.c
@@ -19,52 +19,100 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include QMK_KEYBOARD_H
 #include "txkyel.h"
 
+#define MO_NAV MO(NAV)
+#define MO_NUM MO(NUM)
+
+#define CT_BSPC TD(HC_BSPC)
+#define CT_LEFT TD(HC_LEFT)
+#define CT_RGHT TD(HC_RGHT)
+
+enum layers {
+    BASE,
+    NUM,
+    NAV,
+    GAME,
+};
+
+enum keycodes {
+    // Custom oneshot mod implementation with no timers.
+    OS_SHFT = SAFE_RANGE,
+    OS_CTRL,
+    OS_ALT,
+    OS_GUI,
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [0] = LAYOUT_split_3x6_3(
+  [BASE] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-   TD(HC_DEL),    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,TD(HC_BSPC),
+       KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, CT_BSPC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC,
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                        KC_LGUI,TO(0), KC_SPC, LSFT_T(KC_ENT),OSL(1), KC_LALT
-                                      //`--------------------------'  `--------------------------'
-  ),
-  [1] = LAYOUT_split_3x6_3(
-  //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, XXXXXXX, XXXXXXX, _______,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, KC_LBRC, KC_LCBR, KC_BSLS,  KC_GRV,  KC_EQL,                      KC_MINS, KC_LPRN, XXXXXXX,   TO(3), _______, _______,
-  //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, KC_RBRC, KC_RCBR, KC_PIPE, KC_TILD, KC_PLUS,                      KC_UNDS, KC_RPRN, _______, _______, _______, _______,
-  //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          _______, _______, _______,    _______,   TO(2), _______
+                                           KC_ENT,  MO_NUM,  KC_SPC,    KC_LSFT,  MO_NAV,  KC_ESC
                                       //`--------------------------'  `--------------------------'
   ),
 
-  [2] = LAYOUT_split_3x6_3(
+  [NUM] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, _______,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, KC_LBRC, KC_LCBR, KC_BSLS,  KC_GRV,  KC_EQL,                      KC_MINS, KC_LPRN, XXXXXXX, XXXXXXX, _______, _______,
+      _______, XXXXXXX, KC_LBRC, KC_LCBR,  KC_GRV,  KC_EQL,                      KC_MINS, KC_LPRN, KC_BSLS, KC_PIPE, _______, _______,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______, KC_RBRC, KC_RCBR, KC_PIPE, KC_TILD, KC_PLUS,                      KC_UNDS, KC_RPRN, _______, _______, _______, _______,
+      _______, XXXXXXX, KC_RBRC, KC_RCBR, KC_TILD, KC_PLUS,                      KC_UNDS, KC_RPRN, _______, _______, _______, _______,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          _______, _______, _______,    _______, XXXXXXX, _______
+                                          _______, _______, _______,    _______, _______, _______
                                       //`--------------------------'  `--------------------------'
   ),
 
-  [3] = LAYOUT_split_3x6_3(
+  [NAV] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_HOME,   KC_UP,  KC_END, KC_PGUP, _______,
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                      KC_PSCR, KC_HOME,   KC_UP,  KC_END, KC_PGUP,  KC_DEL,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,TD(HC_LEFT),KC_DOWN,TD(HC_RGHT),KC_PGDN,KC_ENT,
+      _______,  OS_GUI,  OS_ALT, OS_SHFT, OS_CTRL, XXXXXXX,                      XXXXXXX, CT_LEFT, KC_DOWN, CT_RGHT, KC_PGDN,  KC_ENT,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        RESET, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,
+        KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           _______, _______, _______,    _______, _______, _______
                                       //`--------------------------'  `--------------------------'
   )
 };
+
+uint16_t oneshot_cancel_keys[] = {
+    MO_NAV,
+    MO_NUM,
+};
+
+uint16_t oneshot_ignore_keys[] = {
+    MO_NAV, MO_NUM, OS_SHFT, OS_CTRL, OS_ALT, OS_GUI,
+};
+
+oneshot_action_t oneshot_actions[] = {
+    ACTION_ONESHOT(KC_LSFT, OS_SHFT),
+    ACTION_ONESHOT(KC_LCTL, OS_CTRL),
+    ACTION_ONESHOT(KC_LALT, OS_ALT),
+    ACTION_ONESHOT(KC_LGUI, OS_GUI),
+};
+
+bool is_oneshot_cancel_key(uint16_t keycode) {
+    for (int i = 0; i < sizeof(oneshot_cancel_keys) / sizeof(uint16_t); i++) {
+        if (keycode == oneshot_cancel_keys[i]) return true;
+    }
+    return false;
+}
+
+bool is_oneshot_ignored_key(uint16_t keycode) {
+    for (int i = 0; i < sizeof(oneshot_ignore_keys) / sizeof(uint16_t); i++) {
+        if (keycode == oneshot_ignore_keys[i]) return true;
+    }
+    return false;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    for (int i = 0; i < sizeof(oneshot_actions) / sizeof(oneshot_action_t); i++) {
+        update_oneshot(&oneshot_actions[i], keycode, record);
+    }
+    return true;
+}
