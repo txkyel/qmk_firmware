f35280f7551860e4b96dfed3eb1c73b68d0c3395
Kyle Xiao <kylexiao20@gmail.com>
2021-12-22 14:36:08 -0500

    Add Manna Harbour auto mouse buttons for trackpoint

--- keyboards/crkbd/keymaps/txkyel/rules.mk
+++ keyboards/crkbd/keymaps/txkyel/rules.mk
@@ -1,12 +1,13 @@
 LTO_ENABLE          = yes
 # RGBLIGHT_ENABLE     = yes
+# OLED_DRIVER_ENABLE  = yes
 
 # PS2 mouse
 # PS2_MOUSE_ENABLE    = yes
 # PS2_USE_BUSYWAIT    = yes
 # PS2_USE_INT         = yes
-MOUSEKEY_ENABLE     = yes
-# OLED_DRIVER_ENABLE  = yes
+# MOUSEKEY_ENABLE     = yes
+# MH_AUTO_BUTTONS     = yes
 
 TAP_DANCE_ENABLE    = yes
 CALLUM_OS           = yes
